05d23c5b1e344e1196ea969afc0a5cb8b1a80f45
Leonardo Iacovini <leoiacovini@gmail.com>
2016-12-07 02:59:39 -0200

    Fix tests

--- test/poli_auth/token_test.clj
+++ test/poli_auth/token_test.clj
@@ -2,14 +2,16 @@
   (:require [clojure.test :refer :all]
             [poli-auth.logic.token :as token]))
 
-(def user {:id 1
-           :name "user"
-           :email "user@teste.com"
-           :password "1234"})
+(def user {:user/id 1
+           :user/name "user"
+           :user/email "user@teste.com"
+           :user/password "1234"})
 
 (deftest create-token
   (testing "we can create a new token given an user and then decode it"
     (let [token (token/create-token user)
           decoded-token (token/decode-token token)]
+      (println token)
+      (println decoded-token)
       (is (= (:user-id decoded-token) 1))
       (is (= (:iss decoded-token) "poli-auth")))))
